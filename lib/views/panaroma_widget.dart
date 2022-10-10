bb4361eb1a6522274941adc5897d8aa2eccae59e
PavanBhat <pavan.bhat@thewebpeople.in>
2022-10-10 22:29:52 +0530

    Created  panaroma widget

---
+++ lib/views/panaroma_widget.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:panorama/panorama.dart';
+import 'package:take_save_display_12/views/hotspot_button.dart';
+import 'package:take_save_display_12/views/panaroma_network.dart';
+
+class PanoramaWidget extends StatelessWidget {
+  final File myFile;
+  const PanoramaWidget({Key? key, required this.myFile}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+          child: Panorama(
+              animSpeed: 1.0,
+              sensorControl: SensorControl.Orientation,
+              child: Image.file(myFile),
+              hotspots: [
+            Hotspot(
+              latitude: -15.0,
+              longitude: -129.0,
+              width: 300,
+              height: 300,
+              widget: HotSpotButton(
+                  text: "Explore ",
+                  icon: Icons.open_in_browser,
+                  onPressed: () => {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const PanoramaNetwork(),
+                          ),
+                        )
+                      }),
+            ),
+          ])),
+    );
+  }
+}
