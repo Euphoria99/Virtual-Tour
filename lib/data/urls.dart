7603288156cd18af01ab025bc51622d069464221
PavanBhat <pavan.bhat@thewebpeople.in>
2022-10-10 22:27:56 +0530

    Added webp urls for image source

---
+++ lib/data/urls.dart
@@ -0,0 +1,8 @@
+const List<String> urls = [
+  'https://gallery.theta360.guide/media/images/lakemore_retreat_smaller.jpg',
+  'https://gallery.theta360.guide/media/images/chao_huang.jpg',
+  'https://gallery.theta360.guide/media/images/Jason-Sievers-Funky-Taco_smaller.jpg',
+  'https://gallery.theta360.guide/media/images/silicon-blossoms-full-size-50-percent.webp',
+  'https://gallery.theta360.guide/media/images/kai-moorea4_iKnUMV7.webp',
+  'https://gallery.theta360.guide/media/images/skatepark-full-size-50-percent.webp',
+];
